068c028d52f24ff5cd55dd97a2871c15913e234a
Landon Marder <landon.marder@gmail.com>
2017-05-11 16:25:26 -0400

    add specs for deck

---
+++ test/deck_test.exs
@@ -0,0 +1,12 @@
+defmodule Blackjack.DeckTest do
+  use ExUnit.Case, async: true
+  alias Blackjack.Deck
+
+  test "evaluates there are 52 cards in the deck" do
+    assert length(Deck.all) == 52
+  end
+
+  test "evaluates that the deck can shuffle" do
+    assert Deck.shuffle != Deck.shuffle
+  end
+end

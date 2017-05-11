8ee95aee421220d24eae693d66b1bbf5acb468c9
Landon Marder <landon.marder@gmail.com>
2017-05-11 16:32:24 -0400

    update verbiage in tests

--- test/deck_test.exs
+++ test/deck_test.exs
@@ -2,11 +2,11 @@ defmodule Blackjack.DeckTest do
   use ExUnit.Case, async: true
   alias Blackjack.Deck
 
-  test "evaluates there are 52 cards in the deck" do
+  test "that there are 52 cards in the deck" do
     assert length(Deck.new) == 52
   end
 
-  test "evaluates that the deck can shuffle" do
+  test "that the deck can shuffle" do
     assert Deck.shuffle != Deck.shuffle
   end
 end
